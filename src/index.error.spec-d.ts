6910ab35709599ae7e875f5d64be9daef1b0d73d
Chabib N. <chabib.nr@gmail.com>
2026-04-08 14:48:29 +0700

    wip

--- src/index.error.spec-d.ts
+++ src/index.error.spec-d.ts
@@ -1,8 +1,51 @@
+const { foo, bar } = create({
+  foo: {
+    color: 'green',
+    fontSize: 'blue',
+    backgroundColor: {
+      default: 'red',
+      ':focus': 'orange',
+    },
+  },
+  bar: () => ({
+    color: 'red',
+  }),
+})
+
+export const sx = style(
+  {
+    color: 'blue',
+    cornerShape: () => 'round',
+    // fontSize: 'orange',
+    // '::placeholder': {
+    //   color: {
+    //     //
+    //   },
+    // },
+    backgroundColor: {
+      default: 'red',
+      ':focus': 'blue',
+    },
+  },
+  {
+    color: 'red',
+  },
+  foo,
+)
+
+type Foo = StyleXStyles<{
+  color?: 'red' | 'blue' | (string & {})
+  cornerShape?: string
+  fontSize?: string
+  backgroundColor?: string
+}>
+
+export const styled: Foo = [sx, bar()]
+
 test(styler, () => {
   styler({
     color: 'red',
   })
-
   styler({
     color: () => 'red',
   })
@@ -100,7 +143,6 @@ test(styler, () => {
       '--size': {
         //
       },
-
       '::before': {
         backgroundColor: '',
 
@@ -115,37 +157,22 @@ test(styler, () => {
         },
         //
       },
-      ':after': {
-        backgroundColor: '',
-        '--size': {
-          ':focus': {
-            //
-          },
-          //
-        },
-        color: {
-          ':focus': {
-            //
-          },
-          //
-        },
-      },
-      ':-moz-placeholder': {
-        backgroundColor: '',
-        '--size': {
-          ':focus': {
-            //
-          },
-          //
-        },
-        color: {
-          ':focus': {
-            //
-          },
-          //
-        },
-      },
       '::slotted()': {
+        backgroundColor: '',
+        '--size': {
+          ':focus': {
+            //
+          },
+          //
+        },
+        color: {
+          ':focus': {
+            //
+          },
+          //
+        },
+      },
+      '::placeholder': {
         backgroundColor: '',
         '--size': {
           ':focus': {
@@ -169,47 +196,93 @@ test(styler, () => {
     quux: '',
   })
 
-  styler({
-    '::placeholder': {
+  createTheme(vars, {
+    foo: types.integer(1),
+  })
+
+  const sheet = create({
+    test: {
       color: 'red',
+      [vars.foo]: '',
+      textAlign: 'justify',
+    },
+  })
+
+  styler(sheet.test, {
+    '::placeholder': {
+      [vars.foo]: {
+        default: vars.bar,
+      },
       [vars.bar]: 1,
+      [vars.quux]: () => ({
+        //
+      }),
+    },
+    color: {
+      ':active': {
+        [vars.foo]: {
+          default: vars.bar,
+        },
+        [vars.bar]: 1,
+        [vars.quux]: () => ({
+          //
+        }),
+      },
     },
     // color: 'red',
     // [vars.bar]: 1,
   })
 
   styler({
+    '::placeholder': {
+      [vars.quux]: {},
+    },
     color: 'red',
     // backgroundColor: vars.bar,
     [vars.quux]: '',
   })
 
+  styler({
+    textBoxEdge: vars.foo,
+    textBoxTrim: vars.bar,
+  })
+
+  styler({
+    textAlign: 'justify',
+    color: 'red',
+    backgroundColor: vars.bar,
+    textBoxEdge: vars.foo,
+    textBoxTrim: vars.bar,
+    cornerShape: vars.quux,
+    cue: vars.bar,
+    '--foo': vars.foo,
+    '--bar': vars.bar,
+    '--quux': 'quux',
+    '--alpha': {
+      default: vars.foo,
+    },
+  })
+
   expect(() =>
     styler({
-      // textAlign: 'justify',
-      // color: 'red',
-      // backgroundColor: vars.bar,
-      // textBoxEdge: vars.foo,
-      // textBoxTrim: vars.bar,
-      // cornerShape: vars.quux,
-      // cue: vars.bar,
-      // '--foo': vars.foo,
-      // '--bar': vars.bar,
-      // '--quux': 'quux',
-      // '--alpha': {
-      //   default: vars.foo,
-      // },
       [vars.foo]: {
         default: vars.bar,
       },
       [vars.bar]: 1,
+      [vars.quux]: () => ({
+        //
+      }),
     }),
   ).toThrow()
 })
 
+import { create } from '@stylexjs/stylex'
+import { createTheme } from '@stylexjs/stylex'
 import { defineVars } from '@stylexjs/stylex'
 import { expect } from 'vitest'
+import { style } from './index.ts'
 import { styler } from './index.ts'
+import type { StyleXStyles } from '@stylexjs/stylex'
 import { test } from 'vitest'
 import { types } from '@stylexjs/stylex'
 //
