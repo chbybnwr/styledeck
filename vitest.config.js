bd3c4f7da94979d1b59de045f0b90b15681fb836
Chabib N. <chabib.nr@gmail.com>
2026-07-31 03:39:15 +0700

    test: update config

--- vitest.config.js
+++ vitest.config.js
@@ -13,6 +13,7 @@ const vitestConfig = {
     include: [
       '**/*.spec.?(c|m)[jt]s?(x)',
       '**/*.error.spec-d.?(c|m)[jt]s?(x)',
+      '**/*.manual.spec-d.?(c|m)[jt]s?(x)',
       //
     ],
     typecheck: {
